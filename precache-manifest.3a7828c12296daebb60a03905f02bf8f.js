9112f4825fc5b98fdbe8362715edab0ac8ed1a8f
Michael Ainsa <mike@Peregrine.local>
2019-11-27 20:27:45 -0700

    Updates

---
+++ precache-manifest.3a7828c12296daebb60a03905f02bf8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "163f4482c8a6f1826b56a59a1024372c",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "61a7d9542e9e7d9eadc7",
+    "url": "/rolodex/static/css/main.3899dbaf.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "61a7d9542e9e7d9eadc7",
+    "url": "/rolodex/static/js/main.c8e102dd.chunk.js"
+  },
+  {
+    "revision": "b2e3147b89f06d666e8b",
+    "url": "/rolodex/static/js/runtime-main.5a0d0b68.js"
+  }
+]);
